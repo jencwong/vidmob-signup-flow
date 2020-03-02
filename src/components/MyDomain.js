7d7a371f4f56c9c790ac954054a4074b5fd6d7aa
Jenwng <Jenniescwong@gmail.com>
2020-03-02 17:57:55 -0500

    get the carriage return to work behind div

--- src/components/MyDomain.js
+++ src/components/MyDomain.js
@@ -4,9 +4,12 @@ class MyDomain extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      domains: []
+      domains: [],
+      inputDomain: ""
     };
     this.handleChange = this.handleChange.bind(this);
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+    this.deleteDomain = this.deleteDomain.bind(this);
   }
 
   handleChange = event => {
@@ -22,79 +25,142 @@ class MyDomain extends Component {
     }
   };
 
+  //   inputDomain = event => {
+  //     let val = event.target.value;
+  //     if (event.key === val && "Enter") {
+  //       if (this.state.domains.find(domain => domain.toLowerCase() === val)) {
+  //         return;
+  //       }
+  //       let newDomains = [...this.props.domains, val];
+  //       this.setState({ domains: newDomains });
+  //       this.MyDomain.value = null;
+  //       this.props.onDomainChange(newDomains);
+  //     } else if (event.key === !val && "Backspace") {
+  //       this.deleteDomain();
+  //     }
+  //   };
+
   deleteDomain = index => {
     let newDomains = [...this.props.domains];
     newDomains.splice(index, 1);
     this.setState({ domains: newDomains });
-    this.props.onDomainChange(newDomains);
+    this.props.onDomainsChange(newDomains);
   };
 
-  myList() {
-    return this.props.domains.map((domain, index) => {
-      let editableDomain;
-      if (index === this.props.domains.length - 1) {
-        editableDomain = "last-line-item";
-      } else {
-        editableDomain = "";
-      }
-      return (
-        <li className={"domain-list" + editableDomain} key={domain}>
-          <div className="domain-list-containter">
-            <div className="domain-list-text">{domain}</div>
-            <div
-              className="removeDomain-icon"
-              onClick={() => {
-                this.deleteDomain(index);
-              }}
-            >
-              <svg width="32" height="32" xmlns="http://www.w3.org/2000/svg">
-                <g>
-                  <title>background</title>
-                  <rect
-                    fill="none"
-                    id="canvas_background"
-                    height="402"
-                    width="582"
-                    y="-1"
-                    x="-1"
-                  />
-                </g>
-                <g>
-                  <title>Layer 1</title>
-                  <path
-                    fill="#e5e5e5"
-                    id="svg_1"
-                    d="m16,4c-6.617,0 -12,5.383 -12,12s5.383,12 12,12s12,-5.383 12,-12s-5.383,-12 -12,-12m0,-2c7.732,0 14,6.268 14,14s-6.268,14 -14,14s-14,-6.268 -14,-14s6.268,-14 14,-14l0,0zm3.536,9.05l1.414,1.414l-3.536,3.536l3.536,3.536l-1.414,1.414l-3.536,-3.536l-3.536,3.536l-1.414,-1.414l3.536,-3.536l-3.536,-3.536l1.414,-1.414l3.536,3.536l3.536,-3.536z"
-                  />
-                </g>
-              </svg>
-            </div>
-          </div>
-        </li>
-      );
-    });
-  }
+  handleKeyDown = event => {
+    event.preventDefault();
+    // if pressed Enter key
+    if (event.key === "Enter") {
+      // get current list of domains and add a new input to the array
+      let newDomains = [...this.props.domains, this.state.inputDomain];
 
-  render() {
+      // reset the input box and state
+      this.setState({
+        inputDomain: ""
+      });
+      this.props.onDomainsChange(newDomains);
+    }
+
+    // if pressed Backspace
+    else if (event.key === "Backspace") {
+      // reset the input box and state
+      let change = {};
+      change[event.target.name] = "";
+      this.setState(change);
+    }
+    // if pressed shift key
+    else if (event.key === "Shift") {
+      let change = {};
+      change[event.target.name] = "";
+    }
+
+    // if pressed any other keys
+    else {
+      // update input box and state
+      let change = {};
+      change[event.target.name] = event.target.value + event.key;
+      this.setState(change);
+    }
+  };
+
+  myDomain() {
     return (
       <div className="MyDomain">
         <ul className="domain_list">
-          {this.props.domainIsInput ? this.myList() : null}
+          {this.props.domains.map((domain, index) => {
+            let editableDomainClass;
+            if (index === this.props.domains.length - 1) {
+              editableDomainClass = "last-line";
+            } else {
+              editableDomainClass = "";
+            }
+            return (
+              <li className={"domain-item" + editableDomainClass} key={domain}>
+                <div className="domain-list-containter">
+                  <div className="domain-list-text">{domain}</div>
+                  <div
+                    className="removeDomain-icon"
+                    onClick={() => {
+                      this.deleteDomain(index);
+                    }}
+                  >
+                    <svg
+                      className="svg-toggle-button"
+                      width="15"
+                      height="15"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                    >
+                      <g>
+                        <title>background</title>
+                        <rect
+                          fill="none"
+                          id="canvas_background"
+                          height="402"
+                          width="582"
+                          y="-1"
+                          x="-1"
+                        />
+                      </g>
+                      <g>
+                        <title>Layer 1</title>
+                        <path
+                          //   className="svg-toggle-button"
+                          fill="#e5e5e5"
+                          id="svg_1"
+                          d="m16,4c-6.617,0 -12,5.383 -12,12s5.383,12 12,12s12,-5.383 12,-12s-5.383,-12 -12,-12m0,-2c7.732,0 14,6.268 14,14s-6.268,14 -14,14s-14,-6.268 -14,-14s6.268,-14 14,-14l0,0zm3.536,9.05l1.414,1.414l-3.536,3.536l3.536,3.536l-1.414,1.414l-3.536,-3.536l-3.536,3.536l-1.414,-1.414l3.536,-3.536l-3.536,-3.536l1.414,-1.414l3.536,3.536l3.536,-3.536z"
+                        />
+                      </g>
+                    </svg>{" "}
+                  </div>
+                </div>
+              </li>
+            );
+          })}
           <input
             className={
               this.props.domains.length === 0
                 ? "domain-default"
-                : "domain-text-input"
+                : "domain-input"
             }
+            value={this.state.inputDomain}
             type="text"
-            placeholder="Enter one or more domain..."
+            placeholder={
+              this.props.domains.length === 0
+                ? "Enter one or more domain..."
+                : ""
+            }
             id="input-box7"
-            name="text"
-            onChange={event => this.handleChange(event)}
+            name="inputDomain"
+            onKeyDown={this.handleKeyDown}
           ></input>
         </ul>
       </div>
     );
+  }
+
+  render() {
+    return <div>{this.props.domainIsInput ? this.myDomain() : null}</div>;
   }
 }
 export default MyDomain;
